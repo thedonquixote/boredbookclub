f080953c0ef8972119af5e5877640fd56b303ce1
River <46585884+thedonquixote@users.noreply.github.com>
2019-03-20 13:26:32 -0700

    update

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import datetime
+import calendar
+
+
+app = Flask(__name__)
+
+datetime_object = datetime.datetime.now()
+pages = 400
+pagef=round(pages * (datetime_object.day)/calendar.monthrange(datetime_object.year,datetime_object.month)[1])
+percentagef=round((pagef/pages)*100)
+@app.route('/')
+@app.route("/")
+def index():
+    return render_template("index.html",percentage=percentagef,page=pagef)
+
+
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
